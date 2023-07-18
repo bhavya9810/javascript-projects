53d58c19de863996f1a3b17e78a4332e2c7ce26f
bhavya9810 <samgta9810@gmail.com>
2023-07-18 15:43:44 +0530

    clock using javascript

---
+++ clock/script.js
@@ -0,0 +1,14 @@
+let mins=document.getElementById('mins')
+let hrs=document.getElementById('hrs')
+let sec=document.getElementById('sec')
+
+
+setInterval(()=>{
+
+let currentTime=new Date();
+
+hrs.innerHTML=(currentTime.getHours()<10?"0":"")+ currentTime.getHours();
+mins.innerHTML=(currentTime.getMinutes()<10?"0":"")+ currentTime.getMinutes();
+sec.innerHTML=(currentTime.getSeconds()<10?"0":"")+ currentTime.getSeconds();
+
+},1000)
